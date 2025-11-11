b9a9c9daad19f6c1388e0fe5dff1581ceae2c00c
ronaldwopara <rwopara2007@gmail.com>
2025-11-10 23:27:23 -0700

    feat: add Tailwind CSS and update dependencies
    - Added Tailwind CSS for styling with base, components, and utilities.
    - Updated package.json to include new dependencies: @tanstack/react-query, axios, react-router-dom, recharts, autoprefixer, postcss, and tailwindcss.
    - Configured Vite to proxy API requests to the backend server.
    - Created postcss.config.js for PostCSS configuration with Tailwind CSS and autoprefixer.
    - Added tailwind.config.js for Tailwind CSS configuration.

--- ai-postsales-frontend/vite.config.js
+++ ai-postsales-frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Send all requests starting with /api ot the rootto the backend
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      
+      '/': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
